01e0422a09a1de92a87c36ba97e764ee01e47f9f
EnzoYuri1 <enzobalieiro6@gmail.com>
2026-09-23 11:48:08 -0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppHeader />
+    <main id="conteudo" class="flex-1">
+      <slot />
+    </main>
+    <AppFooter />
+    <CartDrawer />
+  </div>
+</template>

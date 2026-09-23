eacc5cec0b8724dedea00aa92a60fd8abe5daf91
EnzoYuri1 <enzobalieiro6@gmail.com>
2026-09-23 11:47:57 -0300

    feat: configure nuxt app

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+  app: {
+    head: {
+      htmlAttrs: { lang: 'pt-BR' },
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Playfair+Display:wght@500;600;700&display=swap'
+        }
+      ]
+    }
+  },
+  runtimeConfig: {
+    public: {
+      siteUrl: 'https://cafeaurora.com.br'
+    }
+  }
+})
